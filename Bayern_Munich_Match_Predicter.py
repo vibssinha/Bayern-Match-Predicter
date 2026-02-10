262b891092cbf81b069e2e2b7c24895ca9d3f684
Vibs Sinha <105296730+vibssinha@users.noreply.github.com>
2026-02-10 01:46:24 -0600

    Refactor match prediction logic and update API calls

--- Bayern_Munich_Match_Predicter.py
+++ Bayern_Munich_Match_Predicter.py
@@ -3,35 +3,29 @@ import json
 from datetime import datetime
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
+import numpy as np
 
 url = "https://api.football-data.org/v4/"
 response = requests.get(url=url)
-api_key = "***"
+api_key = "****"
 headers = {"X-Auth-Token": api_key}
 
 if response.status_code == 200:
     print("Error: Unable to reach the football data API")
 
-def get_team_id(team_name, team_list):
-    for team in team_list['teams']:
-        if team['name'] == team_name or team['shortName'] == team_name:
-            return int(team['id'])
-    return None
 
-
-def get_previous_matches(X_train, Y_train, standings, team_list):
-    params = {"status": "FINISHED", "limit": 7}
+def get_previous_matches(X_train, Y_train, standings, year):
+    params = {"status": "FINISHED", "season": year}
     index = 0
     match_data = requests.get(url = f'{url}/teams/5/matches', headers = headers,params=params)
     matches = match_data.json()['matches']
-    for match in reversed(matches):
+    for match in matches:
         if int(match['competition']['id']) != 2002: 
             continue
         X_train_data = []
         if int(match['homeTeam']['id']) == 5:
             X_train_data.append(1)
             opponent_id = int(match['awayTeam']['id'])
-            print(f'The opponent id is {opponent_id}')
             opponent_current_standing = get_current_standing(team_id=opponent_id, standings=standings)
             X_train_data.append(opponent_current_standing)
             if match['score']['winner'] == 'HOME_TEAM':
@@ -78,8 +72,10 @@ def enter_standings():
         current_year = current_year - 1
     index = 0
     param = {'season': f'{current_year}'}
-    standing = requests.get(url = f'{url}competitions/BL1/standings', headers = headers, params=param)
-    return standing.json()
+    current_standing = requests.get(url = f'{url}competitions/BL1/standings', headers = headers, params=param)
+    one_year_past_standings = requests.get(url = f'{url}competitions/BL1/standings', headers = headers, params={'season': f'{current_year - 1}'})
+    two_year_past_standings = requests.get(url = f'{url}competitions/BL1/standings', headers = headers, params={'season': f'{current_year - 2}'})
+    return current_standing.json(), one_year_past_standings.json(), two_year_past_standings.json()
         
 def get_current_standing(team_id, standings):
     for standing in standings['standings'][0]['table']:
@@ -91,23 +87,23 @@ if __name__ == "__main__":
     team = "Bayern"
     teams = requests.get(url = f'{url}/teams/', headers = headers)
     team_list = teams.json()
-    standings = enter_standings()
-    #next_match_team_id = get_next_match(team, team_list)
-    # Win = 2, Draw = 1, Loss = 0
-    # Home = 1, Away = 0
-    
+    current_standings, one_year_past_standing, two_year_past_standing = enter_standings()
+    current_year = datetime.now().year
+    #Win = 2, Draw = 1, Loss = 0
+    #Home = 1, Away = 0
     X_train = []
     Y_train = []
     X_predict = []
     Y_predict = []
-    get_previous_matches(X_train=X_train, Y_train=Y_train, standings=standings, team_list=team_list)
-    print(X_train)
-    print(Y_train)
-    get_next_match(X_predict=X_predict, standings=standings)
-    print(X_predict)
-    model = LogisticRegression()
-    model.fit(X=X_train, y=Y_train)
+    get_previous_matches(X_train=X_train, Y_train=Y_train, standings=two_year_past_standing, year = int(two_year_past_standing['filters']['season']))
+    get_previous_matches(X_train=X_train, Y_train=Y_train, standings=one_year_past_standing, year = int(one_year_past_standing['filters']['season']))
+    get_previous_matches(X_train=X_train, Y_train=Y_train, standings=current_standings, year=int(current_standings['filters']['season']))
 
+    get_next_match(X_predict=X_predict, standings=current_standings)
+    sample_size = len(X_train)
+    weights = np.exp(np.linspace(-2,0,sample_size))
+    model = LogisticRegression()
+    model.fit(X=X_train, y=Y_train, sample_weight=weights)
     prediction = model.predict(X=X_predict)
     print(f"Prediction: {prediction[0]}")
     probability = model.predict_proba(X=X_predict)
@@ -115,3 +111,6 @@ if __name__ == "__main__":
     results['Probability'] = results['Probability'].apply(lambda x: f'{x:.2%}')
     print("\nProbabilities:")
     print(results.to_string(index=False))
+
+
+   
